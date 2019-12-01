7b635acfea47bb7bb99550deb80d7c5f2531aa6e
Youssef Wagieh <joeyoggie@gmail.com>
2019-12-01 11:42:12 +0200

    utilities v1.2

--- utilities/src/main/java/com/joey/utilities/MyGlideAppModule.java
+++ utilities/src/main/java/com/joey/utilities/MyGlideAppModule.java
@@ -1,4 +1,16 @@
 package com.joey.utilities;
 
-public class MyGlideAppModule {
+import android.content.Context;
+import android.util.Log;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyGlideAppModule extends AppGlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setLogLevel(Log.ERROR);
+    }
 }
