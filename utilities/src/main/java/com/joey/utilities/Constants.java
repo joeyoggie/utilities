e28c864f904f97a0860afac42a46f85e1e55b467
Youssef Wagieh <joeyoggie@gmail.com>
2019-12-01 13:14:08 +0200

    mysettings and myapp additions

--- utilities/src/main/java/com/joey/utilities/Constants.java
+++ utilities/src/main/java/com/joey/utilities/Constants.java
@@ -5,4 +5,10 @@ public class Constants {
 
     public static final String PARAMETER_CODE = "code";
     public static final String PARAMETER_MESSAGE = "msg";
+
+
+    public static final String DB_NAME = "main-db";
+
+    //notification channel constant, only for api26+
+    public static final String CHANNEL_ID = "144";
 }
